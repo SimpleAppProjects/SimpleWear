7303c034c608288e3c182d4f8bb150afc340b8b1
Dave Antoine <bryan2894@gmail.com>
2021-07-03 22:02:58 -0400

    SimpleWear: update splash screen

--- mobile/src/main/java/com/thewizrd/simplewear/MainActivity.kt
+++ mobile/src/main/java/com/thewizrd/simplewear/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
