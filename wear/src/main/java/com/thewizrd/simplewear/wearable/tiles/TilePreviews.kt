b82a019a1241aa69806d603560fce7410c5d71ea
Dave Antoine <thewizrd.dev@gmail.com>
2024-04-06 23:31:31 -0400

    MediaPlayerTileLayout: update layout

--- wear/src/main/java/com/thewizrd/simplewear/wearable/tiles/TilePreviews.kt
+++ wear/src/main/java/com/thewizrd/simplewear/wearable/tiles/TilePreviews.kt
@@ -148,6 +148,7 @@ fun DashboardNotInstalledTilePreview() {
 
 @OptIn(ExperimentalHorologistApi::class)
 @WearPreviewDevices
+@WearPreviewFontScales
 @Composable
 fun MediaPlayerTilePreview() {
     val context = LocalContext.current
